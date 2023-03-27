79c078511258b3a80b4341f021efb9a038294695
spx01 <>
2023-03-27 22:57:09 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# GTE2 Translation
+
+This repository contains automatically English translated FTB Quests configurations for [\[GTCEu\] GregTech Expert 2](https://www.curseforge.com/minecraft/modpacks/gregtech-expert-2) made with DeepL. Also provides a simple automation script not specific to GTE2.
+
+### How to use:
+
+- Download the latest release from the releases page (or the one corresponding with your GTE2 version)
+- Replace the `ftbquests` folder from your Minecraft's instance `config` folder with the one from the archive
+- Greg out in (slightly broken?) English!
+
+### Running the script
+
+For this you're going to need a [DeepL](https://www.deepl.com) account. The entirety of the questbook as of writing this is around ~120k Japanese characters, so it comfortably fits in the monthly DeepL free tier quota.
+```
+$ mv /path/to/your/ftbutils .
+$ pip3 install -r requirements.txt
+$ DEEPL_AUTH_KEY="<insert your deepl key here>" python3 translate.py
+$ mv ftbutils-tl /path/to/your/ftbutils
+```
+
+To update certain quests without having to re-translate everything (which takes quite a bit), simply delete them from the `ftbutils-tl` folder and run the script again.
---
+++ translate.py
@@ -0,0 +1,71 @@
+import deepl
+import re
+import os
+
+source_path = "ftbquests"
+target_path = "ftbquests-tl"
+
+auth_key = os.environ["DEEPL_AUTH_KEY"]
+tl = deepl.Translator(auth_key)
+
+# this matches every "" sequence that contains a non-ascii unicode character
+# also accounts for escaped \" groups inside the double quotes
+match_jap = r'"((?:\\.|[^\\"\n\r])*)[\u0080-\uffff]((?:\\.|[^\\"\n\r])*)"'
+
+
+# replace matches of the r regex in s with f(match)
+def regex_replace(r, s, f):
+    new = ""
+    start = 0
+    for m in re.finditer(r, s):
+        end, newstart = m.span()
+        new += s[start:end]
+        new += f(m.group())
+        start = newstart
+    new += s[start:]
+    return new
+
+
+def copy_structure(src, dst):
+    for dirpath, dirnames, _ in os.walk(src):
+        for d in dirnames:
+            os.makedirs(
+                os.path.join(dst, os.path.join(*(dirpath.split(os.path.sep)[1:]), d)),
+                exist_ok=True,
+            )
+
+
+def translate(s):
+    # un-escape the string so that it doesnt confuse deepl
+    res = s[1:-1].replace('\\"', '"')
+    res = tl.translate_text(res, source_lang="JA", target_lang="EN-US").text
+    return '"' + res.replace('"', '\\"') + '"'
+
+
+def create_file_set(path, f=lambda s: s.endswith(".snbt")):
+    res = set()
+    for dirpath, _, filenames in os.walk(path):
+        for filename in filenames:
+            if not f(filename):
+                continue
+            file = os.path.join(dirpath, filename)
+            if os.path.isfile(file):
+                res.add(os.path.join(*(file.split(os.path.sep)[1:])))
+    return res
+
+
+def main():
+    copy_structure(source_path, target_path)
+    ofiles = create_file_set(source_path)
+    tlfiles = create_file_set(target_path)
+    to_translate = ofiles - tlfiles
+    for fp in to_translate:
+        with open(os.path.join(source_path, fp), "r") as f:
+            s = f.read()
+            print(f"translating {fp}...")
+            translated = regex_replace(match_jap, s, translate)
+            with open(os.path.join(target_path, fp), "x") as tf:
+                tf.write(translated)
+
+
+main()
